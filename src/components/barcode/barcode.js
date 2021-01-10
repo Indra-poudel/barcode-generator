98d6f5b6b4dbbc537344a7e74406edaa6838eb9f
Indra-poudel <indrapoudel1998@gmail.com>
2021-01-10 20:58:06 +0545

    added number on top of square

--- src/components/barcode/barcode.js
+++ src/components/barcode/barcode.js
@@ -18,13 +18,21 @@ const Barcode = (props) => {
         {binaryNumber.split('').map((binaryDigit) => {
           const calculatedWidth = BARCODE_CONTAINER_WIDTH / binaryNumber.length;
           return (
-            <div
-              style={{
-                display: 'inline-block',
-                height: calculatedWidth,
-                width: calculatedWidth,
-                backgroundColor: Boolean(parseInt(binaryDigit)) ? 'black' : 'white',
-              }}></div>
+            <div className="barcode-square-wrapper">
+              <p
+                style={{
+                  fontSize: calculatedWidth > 48 ? 48 : calculatedWidth,
+                }}>
+                {binaryDigit}
+              </p>
+              <div
+                style={{
+                  display: 'inline-block',
+                  height: calculatedWidth,
+                  width: calculatedWidth,
+                  backgroundColor: Boolean(parseInt(binaryDigit)) ? 'black' : 'white',
+                }}></div>
+            </div>
           );
         })}
       </div>
