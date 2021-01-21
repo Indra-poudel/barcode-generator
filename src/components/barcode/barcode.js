356b8f280395ac5f726c1481bda51da4c8bd9c03
Indra-poudel <indrapoudel1998@gmail.com>
2021-01-21 21:44:51 +0545

    2 boxes added

--- src/components/barcode/barcode.js
+++ src/components/barcode/barcode.js
@@ -13,28 +13,103 @@ const Barcode = (props) => {
       <div
         className="barcode-view"
         style={{
-          width: BARCODE_CONTAINER_WIDTH,
+          display: 'flex',
+          justifyContent: 'center',
         }}>
-        {binaryNumber.split('').map((binaryDigit) => {
-          const calculatedWidth = BARCODE_CONTAINER_WIDTH / binaryNumber.length;
-          return (
-            <div className="barcode-square-wrapper">
-              <p
-                style={{
-                  fontSize: calculatedWidth > 48 ? 48 : calculatedWidth,
-                }}>
-                {binaryDigit}
-              </p>
+        <div
+          className="barcode-square-wrapper"
+          style={{
+            display: 'flex',
+            alignItems: 'flex-end',
+          }}>
+          <div
+            style={{
+              marginRight: (BARCODE_CONTAINER_WIDTH / (binaryNumber.length / 2)) * 0.1,
+              display: 'flex',
+              alignSelf: 'flex-end',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: (BARCODE_CONTAINER_WIDTH / binaryNumber.length) * 0.5,
+              width: (BARCODE_CONTAINER_WIDTH / binaryNumber.length) * 0.5,
+              backgroundColor: 'black',
+            }}>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: (BARCODE_CONTAINER_WIDTH / binaryNumber.length) * 0.25,
+                width: (BARCODE_CONTAINER_WIDTH / binaryNumber.length) * 0.25,
+                backgroundColor: 'white',
+              }}>
               <div
                 style={{
+                  margin: 'auto',
                   display: 'inline-block',
-                  height: calculatedWidth,
-                  width: calculatedWidth,
-                  backgroundColor: Boolean(parseInt(binaryDigit)) ? 'black' : 'white',
-                }}></div>
+                  height: (BARCODE_CONTAINER_WIDTH / binaryNumber.length) * 0.125,
+                  width: (BARCODE_CONTAINER_WIDTH / binaryNumber.length) * 0.125,
+                  backgroundColor: 'black',
+                }}
+              />
             </div>
-          );
-        })}
+          </div>
+          {binaryNumber.split('').map((binaryDigit) => {
+            const calculatedWidth = (BARCODE_CONTAINER_WIDTH / binaryNumber.length) * 0.5;
+            return (
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}>
+                <p
+                  style={{
+                    fontSize: calculatedWidth > 48 ? 48 : calculatedWidth,
+                  }}>
+                  {binaryDigit}
+                </p>
+                <div
+                  style={{
+                    display: 'inline-block',
+                    height: calculatedWidth,
+                    width: calculatedWidth,
+                    backgroundColor: Boolean(parseInt(binaryDigit)) ? 'black' : 'white',
+                  }}
+                />
+              </div>
+            );
+          })}
+          <div
+            style={{
+              marginLeft: (BARCODE_CONTAINER_WIDTH / (binaryNumber.length / 2)) * 0.1,
+              display: 'flex',
+              alignSelf: 'flex-end',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: (BARCODE_CONTAINER_WIDTH / binaryNumber.length) * 0.5,
+              width: (BARCODE_CONTAINER_WIDTH / binaryNumber.length) * 0.5,
+              backgroundColor: 'black',
+            }}>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: (BARCODE_CONTAINER_WIDTH / binaryNumber.length) * 0.25,
+                width: (BARCODE_CONTAINER_WIDTH / binaryNumber.length) * 0.25,
+                backgroundColor: 'white',
+              }}>
+              <div
+                style={{
+                  margin: 'auto',
+                  display: 'inline-block',
+                  height: (BARCODE_CONTAINER_WIDTH / binaryNumber.length) * 0.125,
+                  width: (BARCODE_CONTAINER_WIDTH / binaryNumber.length) * 0.125,
+                  backgroundColor: 'black',
+                }}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
